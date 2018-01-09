76d3ddc5a7f55c5b3e707e67dda747ecd0401548
Filip <filipwennerdahl@hotmail.com>
2018-01-09 13:29:16 +0100

    Initial commit

---
+++ Uppgift_4.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+
+	//Yttre länkar öppnas i ett nytt fönster och den aktiva lilla bilden på tab1 markeras.
+	$("a[href^='http://'], a[href^='https://']").attr("target","_blank");
+	$("#filip1").css("opacity", "0.4");
+
+
+	//Funktion som hanterar tab-byte, taben byts och den aktiva fliken markeras.
+	//Taben får en aniemrad storleksbyttning.
+	var lastAttrValue = $(".active a").attr("href");
+	$(".tabs .tab-links a").click(function(e){
+		var currentAttrValue = $(this).attr("href");
+
+		$(".tabs " + currentAttrValue).show().siblings().hide();
+
+		var oldTabHeight = $(".tabs " + lastAttrValue).height();
+		var newTabHeight = $(".tabs " + currentAttrValue).height();
+		var distance = oldTabHeight - newTabHeight;
+		distance = Math.abs(distance);
+
+		if(oldTabHeight != newTabHeight){
+			$(".tabs " + currentAttrValue).find(".content").hide();
+			$(".tabs " + currentAttrValue).height(oldTabHeight);
+			$(".tabs " + currentAttrValue).animate({height: newTabHeight}, {easing: "linear", duration: (distance / 1.5), complete: function(){
+				$(".tabs " + currentAttrValue).find(".content").fadeIn(250);
+				}
+
+			});
+		}
+
+		$(this).parent("li").addClass("active").siblings().removeClass("active");
+		lastAttrValue = $(this).attr("href");
+		e.preventDefault();
+	});
+
+	//Byter bild på tab1, när en liten bild klickas byts bilden i stora ramen ut mot den som är tryckt på i lilla
+	var selectedImage = $("#filip1");
+	$(".imageFilip").click(function(){
+		var imageClicked = $(this).attr("src");
+		var mainImage = $("#mainFilip");
+		if(mainImage.attr("src") != imageClicked){
+			selectedImage.fadeTo("fast", 1);
+			selectedImage = $(this);
+			selectedImage.fadeTo("fast", 0.4);
+			mainImage.fadeOut("fast", function(){
+				mainImage.attr("src", imageClicked);
+				mainImage.fadeIn("fast");
+			});
+		}
+	});
+
+
+	//Variabler som behövs för att dynamiskt ändra storleken på en tab
+	var innerImage;
+	var originalHeight = $(".image").height();
+	var originalWidth = $(".image").width();
+	var tab = $("#tab2");
+	var tabHeight = tab.height();
+
+	//Vid tryck så animeras bilden och blir större samtidigt som en gif spelas upp. Storleken på en tab ändras också när bilden blir större.
+	//Vid ett ytterligare tryck slutar gifen spela och bilden krymper igen.
+	$(".image").click(function(){
+		innerImage = $(this).find(".innerImage");
+		if(!$(this).is(":animated") && $(this).width() == originalWidth){
+			$(this).css("cursor", "zoom-out");
+			$(this).animate({height: innerImage.height(), width: innerImage.width()}, {duration: 200, complete: function() {
+				if(innerImage.attr("id") == "imageRocket"){
+					innerImage.attr("src", "Bilder/RocketMoving.gif");
+				} else if(innerImage.attr("id") == "imageBlow"){
+					innerImage.attr("src", "Bilder/BlowMoving.gif");
+				}
+				
+				}
+			
+			});
+			tab.animate({height: tabHeight + (innerImage.height() - originalHeight)}, {duration: 200});
+			
+		} else if($(this).width() != originalWidth){
+			$(this).animate({height: originalHeight, width: originalWidth}, {duration: 200, complete: function() {
+				$(this).css("cursor", "zoom-in");
+				ResetGif(innerImage.attr("id"));
+		}
+		
+		});
+	
+		tab.animate({height: tabHeight}, {duration: 200});
+		}
+	});
+
+
+	//Funktion som håller koll på återställning av gifen som står still.
+	var gifRocket = $("#imageRocket");
+	var gifBlow = $("#imageBlow");
+	function ResetGif(id){
+		if(id == "imageRocket"){
+			gifRocket.attr("src", "Bilder/RocketStill.gif");
+		} else if(id == "imageBlow"){
+			gifBlow.attr("src", "Bilder/BlowStill.gif");
+		}
+	}
+
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Bakgrundsbild:
+https://drive.google.com/uc?export=download&id=0Bw9PVXgYHxbLTlhqX1RkRjJuc0U
