8774450f902efaf68e52340d31565c9001797985
Filip Wennerdahl <filipwennerdahl@hotmail.com>
2021-06-08 22:31:40 +0200

    Updated the experience view structure

--- src/components/content/experience.js
+++ src/components/content/experience.js
@@ -3,51 +3,88 @@ import React from "react";
 
 import "./experience.scss";
 
-const employments = [
-	{
-		key: Math.random(),
-		location: "DigiExam, Stockholm",
-		date: "Jan 2019 - Present",
-		description: "I am currently employed as a full stack developer where my assignments include everything from developing the front- and back-end of our web application as " +
-		"well as our native client application. The front-end technologies are mainly comprised of JavaScript using a hybrid AngularJS/Angular app (we are currently in the " +
-		"process of moving to the latest version of Angular), the back-end of Go using App Engine and GCP as our host. The back-end work also includes maintaining and developing " +
-		"new REST APIs. Developing the native student clients includes working with C++, Objective-C++, Electron and chromeOS since we support Mac, PC, Chromebook and iOS. " +
-		"Our latest addition is an Android app using Kotlin as our native interface and a WebView to host the core application. I did the main specification and research " + 
-		"for the Android project."
-	},
-	{
-		key: Math.random(),
-		location: "Kentor / Sopra Steria, Stockholm",
-		date: "Jun 2017 – Dec 2018",
-		description: "Employed as an IT consultant where my main assignment was located at Telia. At Telia my work included automated test framework development and management " +
-		"for browser and smartphone testing, development of test PoC’s, automated test maintenance for the Telia Play+ service, etc. Outside of the Telia assignment I also " +
-		"worked on in house projects such as maintenance of a patient management system for dental care built with ASP.NET and assisting in R&D for VR/AR applications."
+class Exp {
+	constructor(location, date, description) {
+		this.key = Math.random();
+		this.location = location
+		this.date = date
+		this.description = description
 	}
-]
+}
+
+const employments = [
+	new Exp(
+		"DigiExam, Stockholm",
+		"Jan 2019 - Present",
+		[
+			{
+				key: Math.random(),
+				paragraph: "I am currently employed as a full stack developer where my assignments include everything from developing the front- and back-end of our web application as well as our native client applications."
+			},
+			{
+				key: Math.random(),
+				paragraph: "The front-end technologies are mainly comprised of JavaScript using a hybrid AngularJS/Angular app (we are currently in the process of moving to the latest version of Angular), the back-end of Go using App Engine and GCP as our host. The back-end work also includes maintaining and developing new REST APIs."
+			},
+			{
+				key: Math.random(),
+				paragraph: "Developing the native client applications includes working with C++, Objective-C++, Electron and chromeOS since we support Mac, PC, Chromebook and iOS. The latest addition is an Android app using Kotlin and several WebViews, this is a project where I lead research and specification."
+			}
+		]
+	),
+	new Exp(
+		"Kentor / Sopra Steria, Stockholm",
+		"Jun 2017 – Dec 2018",
+		[
+			{
+				key: Math.random(),
+				paragraph: "Employed as an IT consultant where my main assignment was located at Telia. At Telia my work included automated test framework development and management for browser and smartphone testing, development of test PoC’s, automated test maintenance for the Telia Play+ service, etc."
+			},
+			{
+				key: Math.random(),
+				paragraph: "Outside of the Telia assignment I also worked on in house projects such as maintenance of a patient management system for dental care built with ASP.NET and assisting in R&D for VR/AR applications."
+			}
+		]
+	)
+];
 
 const educations = [
-	{
-		key: Math.random(),
-		location: "Bachelor’s degree in Computer Science (180p), Stockholm University",
-		date: "Aug 2014 – Jun 2017",
-		description: "Orientation game development – construction. The focus of the education was primarily system development during game development. The majority of " +
-		"the courses had a focus on programming in Java and C++ as well as system science but also modeling through use case- and UML diagrams and SQL database management. " +
-		"The university education also provided experience of data structure- and algorithm analysis as well as test-driven development using Java, it consisted of " +
-		"unit-, integration- and systemtesting."
-	}
-]
+	new Exp(
+		"Bachelor’s degree in Computer Science (180p), Stockholm University",
+		"Aug 2014 – Jun 2017",
+		[
+			{
+				key: Math.random(),
+				paragraph: "A 3 year program with focus on game development to learn the full process of creating a game from the design to release to end users."
+			},
+			{
+				key: Math.random(),
+				paragraph: "The majority of the courses had a focus on programming in Java and C++ as well as system science but also modeling through use case- and UML diagrams and SQL database management."
+			},
+			{
+				key: Math.random(),
+				paragraph: "The university education also provided experience of data structure- and algorithm analysis as well as test-driven development using Java, it consisted of unit-, integration- and systemtesting."
+			}
+		]
+	)
+];
 const educationsIndexing = employments.length % 2 === 0 ? 1 : 0;
 
 const projects = [
-	{
-		key: Math.random(),
-		location: "Jul 2020 - Continuous",
-		date: "Digital portfolio/cv, react",
-		description: "This digital portfolio/cv is my first react project where I've mainly worked with AngularJS and Angular before. I wanted to try something new and decided " +
-		"use react to rebuild my online portofilo, the development took a few days without any major hickups. In the end I did like the process of working with react, " +
-		"especially for a small perosnal project it was pretty great!."
-	}
-]
+	new Exp(
+		"Jul 2020 - Ongoing project",
+		"Digital portfolio/cv, react",
+		[
+			{
+				key: Math.random(),
+				paragraph: "This digial portfolio/cv was an experiment by me to take on React, both to see it's potential but also to expand my toolbox."
+			},
+			{
+				key: Math.random(),
+				paragraph: "If you are curious and want to check out the source you can find the repository on my GitHub profile. I try to keep it updated when I feel there is something new to add or when I get a spark of inspiration."
+			}
+		]
+	)
+];
 const projectsIndexing = educations.length % 2 === 0 ? 1 : 0;
 
 export default class Experience extends React.Component {
@@ -67,29 +104,33 @@ export default class Experience extends React.Component {
 
 					<div className="experienceSection_content containerBGDown">
 						<div className={"textContainer "  + (this.props.currentPage === PAGE.EXPERIENCE ? "textContainer-visible" : "")}>
-							{employment.description}
+							{employment.description.map((descriptionItem) =>
+								<p key={descriptionItem.key}>{descriptionItem.paragraph}</p>
+							)}
 						</div>
 					</div>
 				</div>
 			);
 		});
 
-		const educationItems = educations.map((edication, index) => {
+		const educationItems = educations.map((education, index) => {
 			return(
-				<div key={edication.key} className={`experience_content experienceSection experience_content-${index % educationsIndexing === 0 ? "left" : "right"}`}>
+				<div key={education.key} className={`experience_content experienceSection experience_content-${index % educationsIndexing === 0 ? "left" : "right"}`}>
 					<div className="experienceSection_header">
 						<div>
-							{edication.location}
+							{education.location}
 						</div>
 					
 						<div>
-							{edication.date}
+							{education.date}
 						</div>
 					</div>
 
 					<div className="experienceSection_content containerBGDown">
 						<div className={"textContainer "  + (this.props.currentPage === PAGE.EXPERIENCE ? "textContainer-visible" : "")}>
-							{edication.description}
+							{education.description.map((descriptionItem) =>
+								<p key={descriptionItem.key}>{descriptionItem.paragraph}</p>
+							)}
 						</div>
 					</div>
 				</div>
@@ -111,7 +152,9 @@ export default class Experience extends React.Component {
 
 					<div className="experienceSection_content containerBGDown">
 						<div className={"textContainer "  + (this.props.currentPage === PAGE.EXPERIENCE ? "textContainer-visible" : "")}>
-							{project.description}
+							{project.description.map((descriptionItem) =>
+								<p key={descriptionItem.key}>{descriptionItem.paragraph}</p>
+							)}
 						</div>
 					</div>
 				</div>
