b4fc1d7e4c6c9a66b4360820182c43a6cce28596
Filip Wennerdahl <filipwennerdahl@hotmail.com>
2021-06-23 18:01:10 +0200

    Wording

--- src/components/content/experience.js
+++ src/components/content/experience.js
@@ -80,7 +80,7 @@ const projects = [
 			},
 			{
 				key: Math.random(),
-				paragraph: "If you are curious and want to check out the source you can find <a href='https://github.com/FilipWennerdahl/FilipWennerdahl.github.io' target='_blank'>the repository on my GitHub profile</a>. I try to keep it updated when I feel there is something new to add or when I get a spark of inspiration."
+				paragraph: "If you are curious and want to check out the source you can find <a href='https://github.com/FilipWennerdahl/FilipWennerdahl.github.io' target='_blank'>the repository on my GitHub profile</a>. I try to keep it updated when I get a spark of inspiration or when there is new information to add."
 			}
 		]
 	)
